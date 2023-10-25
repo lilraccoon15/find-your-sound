8c93f6ec0e25024f5a428d8b356877cae77ab1cf
LoiseauB <bapt.loiseau@gmail.com>
2023-10-25 17:14:32 +0200

    style: navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,19 +18,20 @@ const Navbar = () => {
 
     return(
         <>
-        <div className="flex justify-between border-b border-purple pt-4 pb-4">
-            <span className="font-extrabold text-[1.5rem] ml-4 w-1/2 uppercase"><NavLink to="/">Recotify</NavLink></span>
-            <nav className="mr-4 w-1/3">
+        <div className="flex justify-between items-center border-b border-purple py-4 px-8 w-screen ">
+            <span className="font-extrabold text-[1.5rem] uppercase"><NavLink to="/">Recotify</NavLink></span>
+            <nav className="w-[22rem] me-20">
                 <ul className="flex justify-between font-bold text-spotify">
                     {user.email !== null ? (
                     <>
-                        <NavLink to="/recommendations"><li>Recommandations</li></NavLink>
-                        <NavLink to="/profile"><li>Profil</li></NavLink>
-                        <li onClick={toggleDropdown} className="cursor-pointer">Compte</li>
+                        <NavLink to="/recommendations"><li className="hover:border-b-[0.15rem] hover:border-spotify">Recommandations</li></NavLink>
+                        <NavLink to="/profile"><li className="hover:border-b-[0.15rem] hover:border-spotify">Profil</li></NavLink>
+                        <li onClick={() => toggleDropdown()} className="cursor-pointer">Compte</li>
                         {isOpen && (
-                            <ul className="absolute top-[70px] right-[80px] border-b border-r border-l border-purple pr-2 pl-2">
-                                <NavLink to="/settings"><li className="border-b border-purple pt-2 pb-2">Paramètres</li></NavLink>
-                                <li onClick={logOut} className="cursor-pointer pt-2 pb-2">Se déconnecter</li>
+                            <ul onMouseLeave={() => toggleDropdown()}  className="absolute top-[70px] right-[80px] border-b border-r border-l border-purple translate-x-9 px-4 py-4 rounded-2xl -translate-y-5 bg-purple-dark">
+                                <NavLink to="/settings"><li className="my-4 text-white">Paramètres</li></NavLink>
+                                <div className="w-full h-[0.05rem] bg-purple"></div>
+                                <li onClick={logOut} className="cursor-pointer my-4 text-white">Se déconnecter</li>
                             </ul>
                         )}
                     </>
